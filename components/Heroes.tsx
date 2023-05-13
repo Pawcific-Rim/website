6cf3764f8d047525a619a2e5ab7e0c51c914e283
0xbbrandon <brandonobryan2701@gmail.com>
2023-05-13 23:38:26 +0700

    :dizzy: feat: animate heroes section

--- components/Heroes.tsx
+++ components/Heroes.tsx
@@ -1,13 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
-import React, { MutableRefObject, useState } from 'react'
-import { twMerge } from 'tailwind-merge'
+import React, { MutableRefObject, useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
+import { motion, useAnimation, useInView, AnimatePresence } from 'framer-motion'
 import {
   useKeenSlider,
   KeenSliderPlugin,
   KeenSliderInstance,
 } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
+
+import { transition } from '@/utils/constant'
 
 function ThumbnailPlugin(
   mainRef: MutableRefObject<KeenSliderInstance | null>
@@ -45,6 +47,35 @@ function ThumbnailPlugin(
   }
 }
 
+const thumbnailVariants = {
+  hidden: { opacity: 0, y: 100, transition: { duration: 0.2 } },
+  visible: { opacity: 1, y: 0, transition },
+}
+
+const leftArrowVariants = {
+  hidden: { opacity: 0, x: -100, y: -100, transition: { duration: 0.2 } },
+  visible: { opacity: 1, x: 0, y: 0, transition },
+}
+
+const rightArrowVariants = {
+  hidden: { opacity: 0, x: 100, y: -100, transition: { duration: 0.2 } },
+  visible: { opacity: 1, x: 0, y: 0, transition },
+}
+
+const mainImageVariants = {
+  hidden: { opacity: 0, y: -300, transition: { duration: 0.2 } },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { ...transition, type: 'spring', duration: 1 },
+  },
+}
+
+const baseGroundVariants = {
+  hidden: { opacity: 0, transition: { duration: 0.2 } },
+  visible: { opacity: 1, transition },
+}
+
 export default function Heroes() {
   const [loaded, setLoaded] = useState(false)
   const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
@@ -54,6 +85,19 @@ export default function Heroes() {
       setLoaded(true)
     },
   })
+  const ref = useRef(null)
+  const isInView = useInView(ref, { margin: '-200px 0px -200px 0px' })
+  const controls = useAnimation()
+
+  console.log('isInView', isInView)
+
+  useEffect(() => {
+    if (isInView) {
+      controls.start('visible')
+    } else {
+      controls.start('hidden')
+    }
+  }, [controls, isInView])
 
   const [thumbnailRef] = useKeenSlider<HTMLDivElement>(
     {
@@ -90,16 +134,32 @@ export default function Heroes() {
           priority={true}
         />
       </picture>
-      <div className="container absolute inset-0 isolate mx-auto mb-[96px] mt-[96px] px-6 md:mt-[120px] lg:max-w-[1440px]">
+      <div
+        ref={ref}
+        className="container absolute inset-0 isolate mx-auto mb-[96px] mt-[96px] px-6 md:mt-[120px] lg:max-w-[1440px]"
+      >
         <div className="mx-auto flex h-full w-full flex-col justify-center px-0 sm:px-6 lg:m-auto lg:w-[95vh]">
           <div className="relative pb-[58px] sm:pb-[89px] lg:pb-[69px]">
-            <h2 className="character-name z-10 mb-2 text-center text-[40px] font-bold capitalize leading-[48px] text-white sm:hidden">
+            <motion.h2
+              variants={baseGroundVariants}
+              animate={controls}
+              className="character-name z-10 mb-2 text-center text-[40px] font-bold capitalize leading-[48px] text-white sm:hidden"
+            >
               Shiba yukata
-            </h2>
-            <p className="mb-[40px] text-center text-base capitalize leading-[32px] text-white sm:hidden">
+            </motion.h2>
+            <motion.p
+              variants={baseGroundVariants}
+              animate={controls}
+              className="mb-[40px] text-center text-base capitalize leading-[32px] text-white sm:hidden"
+            >
               The Strongest hero in Galaxy 109
-            </p>
-            <div ref={sliderRef} className="keen-slider">
+            </motion.p>
+            <motion.div
+              variants={mainImageVariants}
+              animate={controls}
+              ref={sliderRef}
+              className="keen-slider"
+            >
               <div className="keen-slider__slide">
                 <img
                   className="mx-auto"
@@ -135,8 +195,12 @@ export default function Heroes() {
                   alt="Character 5"
                 />
               </div>
-            </div>
-            <div className="absolute -bottom-10 w-full px-8 text-center sm:-bottom-20 lg:-bottom-24">
+            </motion.div>
+            <motion.div
+              variants={baseGroundVariants}
+              animate={controls}
+              className="absolute -bottom-10 w-full px-8 text-center sm:-bottom-20 lg:-bottom-24"
+            >
               <img
                 src="/heroes/light.png"
                 alt="light"
@@ -150,89 +214,100 @@ export default function Heroes() {
               </p>
 
               <img src="/heroes/base.png" alt="base" className="mx-auto" />
-            </div>
+            </motion.div>
           </div>
 
-          <div className="heroes-carousel-mask p-1">
-            <div className="flex items-center rounded-xl bg-gradient-to-b from-[#C1A6F3] to-[#9778CD] py-4">
-              <div className="w-4 sm:hidden" />
-              <button
-                className="mr-4 hidden sm:block sm:max-w-[100px]"
-                onClick={(e: any) =>
-                  e.stopPropagation() || instanceRef.current?.prev()
-                }
-              >
-                <img src="/heroes/arrow-left.png" alt="Arrow left" />
-              </button>
-              <div
-                ref={thumbnailRef}
-                className="keen-slider thumbnail !overflow-visible"
-              >
-                <div className="keen-slider__slide cursor-pointer border-2 border-transparent sm:border-4">
-                  <img
-                    className="h-full w-full"
-                    src="/heroes/char1.png"
-                    alt="Character thumb 1"
-                  />
+          <motion.div
+            variants={thumbnailVariants}
+            animate={controls}
+            className="heroes-carousel-mask p-1"
+          >
+            {loaded && instanceRef.current && (
+              <div className="flex items-center rounded-xl bg-gradient-to-b from-[#C1A6F3] to-[#9778CD] py-4">
+                <div className="w-4 sm:hidden" />
+                <button
+                  className="mr-4 hidden sm:block sm:max-w-[100px]"
+                  onClick={(e: any) =>
+                    e.stopPropagation() || instanceRef.current?.prev()
+                  }
+                >
+                  <img src="/heroes/arrow-left.png" alt="Arrow left" />
+                </button>
+
+                <div
+                  ref={thumbnailRef}
+                  className="keen-slider thumbnail !overflow-visible"
+                >
+                  <div className="keen-slider__slide cursor-pointer border-2 border-transparent sm:border-4">
+                    <img
+                      className="h-full w-full"
+                      src="/heroes/char1.png"
+                      alt="Character thumb 1"
+                    />
+                  </div>
+                  <div className="keen-slider__slide cursor-pointer border-2 border-transparent sm:border-4">
+                    <img
+                      className="h-full w-full"
+                      src="/heroes/char2.png"
+                      alt="Character thumb 2"
+                    />
+                  </div>
+                  <div className="keen-slider__slide cursor-pointer border-2 border-transparent sm:border-4">
+                    <img
+                      className="h-full w-full"
+                      src="/heroes/char3.png"
+                      alt="Character thumb 3"
+                    />
+                  </div>
+                  <div className="keen-slider__slide cursor-pointer border-2 border-transparent sm:border-4">
+                    <img
+                      className="h-full w-full"
+                      src="/heroes/char4.png"
+                      alt="Character thumb 4"
+                    />
+                  </div>
+                  <div className="keen-slider__slide cursor-pointer border-2 border-transparent sm:border-4">
+                    <img
+                      className="h-full w-full"
+                      src="/heroes/char5.png"
+                      alt="Character thumb 5"
+                    />
+                  </div>
                 </div>
-                <div className="keen-slider__slide cursor-pointer border-2 border-transparent sm:border-4">
-                  <img
-                    className="h-full w-full"
-                    src="/heroes/char2.png"
-                    alt="Character thumb 2"
-                  />
-                </div>
-                <div className="keen-slider__slide cursor-pointer border-2 border-transparent sm:border-4">
-                  <img
-                    className="h-full w-full"
-                    src="/heroes/char3.png"
-                    alt="Character thumb 3"
-                  />
-                </div>
-                <div className="keen-slider__slide cursor-pointer border-2 border-transparent sm:border-4">
-                  <img
-                    className="h-full w-full"
-                    src="/heroes/char4.png"
-                    alt="Character thumb 4"
-                  />
-                </div>
-                <div className="keen-slider__slide cursor-pointer border-2 border-transparent sm:border-4">
-                  <img
-                    className="h-full w-full"
-                    src="/heroes/char5.png"
-                    alt="Character thumb 5"
-                  />
-                </div>
+                <div className="w-4 sm:hidden" />
+                <button
+                  className="hidden pl-4 sm:block"
+                  onClick={(e: any) =>
+                    e.stopPropagation() || instanceRef.current?.next()
+                  }
+                >
+                  <img src="/heroes/arrow-right.png" alt="Arrow right" />
+                </button>
               </div>
-              <div className="w-4 sm:hidden" />
-              <button
-                className="hidden pl-4 sm:block"
-                onClick={(e: any) =>
-                  e.stopPropagation() || instanceRef.current?.next()
-                }
-              >
-                <img src="/heroes/arrow-right.png" alt="Arrow right" />
-              </button>
-            </div>
-          </div>
+            )}
+          </motion.div>
           {loaded && instanceRef.current && (
             <div className="mt-5 flex items-center justify-center sm:hidden">
-              <button
+              <motion.button
+                variants={leftArrowVariants}
+                animate={controls}
                 className="mr-6 w-[48px]"
                 onClick={(e: any) =>
                   e.stopPropagation() || instanceRef.current?.prev()
                 }
               >
                 <img src="/heroes/arrow-left.png" alt="Arrow left" />
-              </button>
-              <button
+              </motion.button>
+              <motion.button
+                variants={rightArrowVariants}
+                animate={controls}
                 className="ml-6 w-[48px]"
                 onClick={(e: any) =>
                   e.stopPropagation() || instanceRef.current?.next()
                 }
               >
                 <img src="/heroes/arrow-right.png" alt="Arrow right" />
-              </button>
+              </motion.button>
             </div>
           )}
         </div>
