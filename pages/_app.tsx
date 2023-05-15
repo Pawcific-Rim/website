2dc78fb034758379a109069a2e9e472faa521869
0xbbrandon <brandonobryan2701@gmail.com>
2023-05-15 18:31:16 +0700

    :bug: chore: fix mobile viewport issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { Montserrat, Inter } from 'next/font/google'
+import { useMount } from 'ahooks'
 
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
@@ -10,6 +11,18 @@ const montserrat = Montserrat({
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
 export default function App({ Component, pageProps }: AppProps) {
+  const setViewHeight = () => {
+    let vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+  }
+
+  useMount(() => {
+    setViewHeight()
+    window.addEventListener('resize', () => {
+      setViewHeight()
+    })
+  })
+
   return (
     <main
       className={`${montserrat.variable} ${inter.variable} font-montserrat`}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    height: (theme) => ({
+      auto: 'auto',
+      ...theme('spacing'),
+      full: '100%',
+      screen: 'calc(var(--vh) * 100)',
+    }),
+    minHeight: (theme) => ({
+      0: '0',
+      ...theme('spacing'),
+      full: '100%',
+      screen: 'calc(var(--vh) * 100)',
+    }),
     extend: {
       fontFamily: {
         inter: ['var(--font-inter)'],
