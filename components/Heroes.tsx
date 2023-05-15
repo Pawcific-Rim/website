cf51b1116afadf2b7967cd4443620db5a9ea5a6c
0xbbrandon <brandonobryan2701@gmail.com>
2023-05-16 00:50:18 +0700

    :art: flex-1

--- components/Heroes.tsx
+++ components/Heroes.tsx
@@ -214,7 +214,7 @@ export default function Heroes() {
               <div className="flex items-center rounded-xl bg-gradient-to-b from-[#C1A6F3] to-[#9778CD] py-4">
                 <div className="w-4 sm:hidden" />
                 <button
-                  className="mr-4 hidden sm:block sm:w-[72px] lg:w-[96px]"
+                  className="mr-4 hidden sm:block sm:w-[72px] lg:w-[86px]"
                   onClick={(e: any) =>
                     e.stopPropagation() || instanceRef.current?.prev()
                   }
@@ -224,7 +224,7 @@ export default function Heroes() {
 
                 <div
                   ref={thumbnailRef}
-                  className="keen-slider thumbnail !overflow-visible"
+                  className="keen-slider thumbnail flex-1 !overflow-visible"
                 >
                   <div className="keen-slider__slide cursor-pointer border-2 border-transparent sm:border-4">
                     <img
@@ -264,7 +264,7 @@ export default function Heroes() {
                 </div>
                 <div className="w-4 sm:hidden" />
                 <button
-                  className="hidden pl-4 sm:block sm:w-[72px] lg:w-[96px]"
+                  className="hidden pl-4 sm:block sm:w-[72px] lg:w-[86px]"
                   onClick={(e: any) =>
                     e.stopPropagation() || instanceRef.current?.next()
                   }
